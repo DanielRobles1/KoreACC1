3e26bf0cdb839db6dc0affee57df23cb5aace5d8
Daniel-Robles1 <jovaban43@gmail.com>
2025-09-21 16:39:01 -0600

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,41 +1,25 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-
 import { UsuariosComponent } from './pages/usuarios/usuarios/usuarios.component';
-
 import { LoginComponent } from './pages/login/login.component';
 import { RecuperarcontrComponent } from './pages/recuperarcontr/recuperarcontr.component';
 import { RolesypermisosComponent } from './pages/rolesypermisos/rolesypermisos.component';
 import { CambiarPasswordComponent } from './pages/cambiar-password/cambiar-password.component';
-
+import { AuthGuard } from './guards/auth.guard'; 
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  }, 
-  {
-    path: 'usuarios',
-    component: UsuariosComponent
- 
- },
- {
-  path: 'login/recuperar-password',
-  component: RecuperarcontrComponent
-},
- {
-  path: 'login/roles',
-  component: RolesypermisosComponent
-},
-{ path: 'cambiar-password', component: CambiarPasswordComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'login/recuperar-password', component: RecuperarcontrComponent },
+
+  { path: 'cambiar-password', component: CambiarPasswordComponent },
+
+  // Rutas protegidas con el guard
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'usuarios', component: UsuariosComponent, canActivate: [AuthGuard] },
+  { path: 'login/roles', component: RolesypermisosComponent, canActivate: [AuthGuard] },
+
+  { path: '**', redirectTo: 'login' } 
 ];
